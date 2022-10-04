90feda2f3c90c279ad3a40fb788bb7bb89737271
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-10-04 17:37:19 -0300

    queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,18 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT name from animals
+WHERE name = '%mon';
+SELECT name from animals
+WHERE date_of_birth BETWEEN '2016/01/01' AND '2019/12/31';
+SELECT name from animals
+WHERE neutered = TRUE AND escape_attempts < 3;
+SELECT date_of_birth from animals
+WHERE name = 'Augmon' OR name = 'Pikachu';
+SELECT name, escape_attempts from animals
+WHERE weight_kg > 10.5;
+SELECT * from animals
+WHERE neutered = TRUE;
+SELECT name from animals
+WHERE name != 'Gabumon';
+SELECT * from animals
+WHERE weight_kg >= 10.4 AND weight_kg <= 17.3
