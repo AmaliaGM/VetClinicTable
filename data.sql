a2f24fff0931df8eca1586ae2c18b1b7b07f1958
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-10-04 10:49:32 -0300

    data.sql

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (1,'Augmon', '02/03/2020', 0, true, 10.23);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (2,'Gabumon', '10/15/2018', 2, true, 8);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (3,'Pikachu', '06/07/2021', 1, false, 15.04);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (4,'Devimon', '05/12/2017', 5, true, 11);
