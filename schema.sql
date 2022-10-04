674c4e1a6181235401d22aa5638ff316d70c52d1
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-10-04 10:29:47 -0300

    schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,19 @@
-/* Database schema to keep the structure of entire database. */
+-- Table: public.animals
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+-- DROP TABLE IF EXISTS public.animals;
+
+CREATE TABLE IF NOT EXISTS public.animals
+(
+    id integer NOT NULL,
+    name text COLLATE pg_catalog."default" NOT NULL,
+    date_of_birth date NOT NULL,
+    escape_attempts integer NOT NULL,
+    neutered boolean NOT NULL,
+    weight_kg numeric NOT NULL,
+    CONSTRAINT animals_pkey PRIMARY KEY (id, name)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.animals
+    OWNER to postgres;
