bdac49a8e0c740c64dd71d5586dde9de43701d04
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-10-04 21:02:09 -0300

    data insertions

--- data.sql
+++ data.sql
@@ -4,3 +4,10 @@ INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) V
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (2,'Gabumon', '10/10/2018', 2, true, 8);
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (3,'Pikachu', '06/07/2021', 1, false, 15.04);
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (4,'Devimon', '05/12/2017', 5, true, 11);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (5,'Charmander', '08/02/2020', 0, false, 11);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (6,'Plantmon', '15/10/2021', 2, true, 5.7);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (7,'Squirtle', '02/04/1993', 3, false, 12.13);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (8,'Angemon', '12/06/2005', 1, true, 45);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (9,'Boarmon', '07/06/2005', 7, true, 20.4);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (10,'Blossom', '13/08/1998', 3, true, 17);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (11, 'Ditto', '14/05/2022', 4, true, 22);
