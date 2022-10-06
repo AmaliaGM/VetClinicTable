780ffd2a313124810caea5e549597294216c83be
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-10-06 15:03:41 -0300

    owner table insertions

--- data.sql
+++ data.sql
@@ -9,3 +9,10 @@ INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg, s
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg, species) VALUES (3,'Pikachu', '2021/06/07', 1, false, 15.04);
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg, species) VALUES (4,'Devimon', '2017/05/12', 5, true, 11);
 
+INSERT INTO owners (id, full_name, age) VALUES (1,'Sam Smith', 34);
+INSERT INTO owners (id, full_name, age) VALUES (2, 'Jennifer Orwell', 19);
+INSERT INTO owners (id, full_name, age) VALUES (3, 'Bob', 45);
+INSERT INTO owners (id, full_name, age) VALUES (4, 'Melody Pond', 77);
+INSERT INTO owners (id, full_name, age) VALUES (5, 'Dean Winchester', 14);
+INSERT INTO owners (id, full_name, age) VALUES (6, 'Jodie Whittaker', 38);
+
