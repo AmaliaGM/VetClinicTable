cab360142edcc9fa9c589df8f3750981127d3050
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-10-05 21:01:21 -0300

    dates update

--- data.sql
+++ data.sql
@@ -1,10 +1,10 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg, species) VALUES (1,'Augmon', '02/03/2020', 0, true, 10.23);
-INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg, species) VALUES (2,'Gabumon', '10/10/2018', 2, true, 8);
-INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg, species) VALUES (3,'Pikachu', '06/07/2021', 1, false, 15.04);
-INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg, species) VALUES (4,'Devimon', '05/12/2017', 5, true, 11);
-INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg, species) VALUES (1,'Augmon', '02/03/2020', 0, true, 10.23);
-INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg, species) VALUES (2,'Gabumon', '10/10/2018', 2, true, 8);
-INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg, species) VALUES (3,'Pikachu', '06/07/2021', 1, false, 15.04);
-INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg, species) VALUES (4,'Devimon', '05/12/2017', 5, true, 11);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg, species) VALUES (1,'Augmon', '2020/02/03', 0, true, 10.23);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg, species) VALUES (2,'Gabumon', '2018/10/10', 2, true, 8);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg, species) VALUES (3,'Pikachu', '2021/06/07', 1, false, 15.04);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg, species) VALUES (4,'Devimon', '201705/12', 5, true, 11);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg, species) VALUES (1,'Augmon', '2020/02/03', 0, true, 10.23);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg, species) VALUES (2,'Gabumon', '2018/10/10', 2, true, 8);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg, species) VALUES (3,'Pikachu', '2021/06/07', 1, false, 15.04);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg, species) VALUES (4,'Devimon', '2017/05/12', 5, true, 11);
