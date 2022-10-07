c347f3ed36789e4445d3b45c569ab6b096e90700
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-10-07 17:35:20 -0300

    visit,vet and specialization tables

--- data.sql
+++ data.sql
@@ -105,3 +105,11 @@ COMMIT;
 
 SELECT *
 FROM ANIMALS;
+
+INSERT INTO vets("name",age,date_of_graduation) VALUES('William Tatcher',45,'2000/04/03');
+INSERT INTO vets("name",age,date_of_graduation) VALUES('Maisy Smith',	26,	'2019/06/17');
+INSERT INTO vets("name",age,date_of_graduation) VALUES('Stephanie Mendez',	64,	'1981/05/04');
+INSERT INTO vets("name",age,date_of_graduation) VALUES('Jack Harkness',	38,	'2008/06/08');
+SELECT * FROM vets;
+
+
